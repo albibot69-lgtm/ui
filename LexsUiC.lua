fa8294081d781f0f361f89e83eea1c1c85df7c7f
LexsHubNew <albibot69@gmail.com>
2026-01-21 20:15:56 +0700

    Update LexsUiC.lua

--- LexsUiC.lua
+++ LexsUiC.lua
@@ -1,4 +1,4 @@
-local HttpService = game:GetService("HttpService") -- V0.0.2
+local HttpService = game:GetService("HttpService") -- V0.0.1
 
 if not isfolder("LexsHub") then
     makefolder("LexsHub")
@@ -60,7 +60,7 @@ local Icons = {
     shop      = "rbxassetid://4985385964",
     cart      = "rbxassetid://128874923961846",
     plug      = "rbxassetid://137601480983962",
-    seting  = "rbxassetid://70386228443175",
+    settings  = "rbxassetid://70386228443175",
     loop      = "rbxassetid://122032243989747",
     gps       = "rbxassetid://17824309485",
     compas    = "rbxassetid://125300760963399",
